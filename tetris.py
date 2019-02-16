760347974117c70b8898da4df132dd92bf0f524e
Veky <vedgar+git@gmail.com>
2019-02-16 17:15:00 +0100

    Linije

--- tetris.py
+++ tetris.py
@@ -1,10 +1,10 @@
 import pygame as G, random, enum, os, time, collections, contextlib
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = '200,35'
-
+lijevi_odmak, gornji_odmak = 200, 35
 širina, visina = 10, 15
-q = 30
-cheat_code_activated = False
+margina = 10
+q = 40
+cheat = False
 bodovi = 0
 
 class Oblik(enum.Enum):
@@ -22,26 +22,72 @@ class Oblik(enum.Enum):
                  '###']
     I = ['####']
 
-    def popuni(self, i0, j0):
+##    Ivan = ['  #  ',
+##            ' ### ',
+##            '#####',
+##            ' ### ',
+##            '  #  ']
+
+    def popuni(self, di=0, dj=0):
         opolje = set()
-        for i, linija in enumerate(self.value, start=i0):
-            for j, znak in enumerate(linija, start=j0):
-                if znak == '#':
-                    opolje.add(Blok(i, j, self.boja))
+        for i, linija in enumerate(self.value, start=self.i+di):
+            for j, znak in enumerate(linija, start=self.j+dj):
+                if znak == '#': opolje.add(Blok(i, j, self.boja))
         return opolje
 
-oblici = list(Oblik)
+    def kolizija(self, di=0, dj=0):
+        opolje = self.popuni(di, dj)
+        if opolje.isdisjoint(polje):
+            self.i += di
+            self.j += dj
+        else: return self.popuni()
+
+    def nacrtaj(self):
+        for blok in self.popuni(): blok.nacrtaj()
+
+
+def novi_oblik(oblici = list(Oblik)):
+    o = random.choice(oblici)
+    o.i, o.j = 0, širina // 2 - 1
+    print(*o.value, sep='\n')
+    print('-' * 10)
+    o.boja = random.choice(Boja.za_oblike)
+    if o.kolizija(): raise Kraj
+    dodaj_bodove(1)
+    return o
+
+
+def dodaj_bodove(b):
+    global bodovi
+    bodovi += b
+
+
+def konsolidiraj():
+    polje.update(o.popuni())
+    iznad = set()
+    linije = 0
+    while ...:
+        for i in range(visina):
+            linija = {blok for blok in polje if blok.i == i and blok.unutra()}
+            if len(linija) < širina: iznad |= linija
+            else:
+                dodaj_bodove(100)
+                polje.difference_update(iznad | linija)
+                polje.update(blok.dolje() for blok in iznad)
+                break
+        else: break
+
 
 class Blok(collections.namedtuple('BlokBaza', 'i j boja')):
-    def nacrtaj(self):
-        G.draw.rect(ekran, self.boja, G.Rect(self.j*q + 10, self.i*q + 10, q, q))
-
-    def __hash__(self):
-        return hash((self.i, self.j))
-
-    def __eq__(self, other):
-        return self.i == other.i and self.j == other.j
+    def nacrtaj(self): G.draw.rect(ekran, self.boja,
+        G.Rect(self.j*q + margina, self.i*q + margina, q, q))
+    def pozicija(self): return self.i, self.j
+    def dolje(self): return self._replace(i=self.i + 1)
+    def unutra(self): return self.i < visina and 1 <= self.j <= širina
+    def __hash__(self): return hash(self.pozicija())
+    def __eq__(self, other): return self.pozicija() == other.pozicija()
         
+
 class Boja:
     crvena = G.Color('#ff0000')
     crna = G.Color('black')
@@ -49,95 +95,51 @@ class Boja:
     siva = G.Color('gray')
     žuta = G.Color('yellow')
     narančasta = G.Color('orange')
+    zelena = G.Color('green')
+    za_oblike = [crvena, plava, žuta, narančasta, zelena]
 
-    @classmethod
-    def slučajna(cls):
-        boje = [boja for boja in vars(Boja).values()
-                if isinstance(boja, G.Color)
-                and boja not in [Boja.crna, Boja.siva]]
-        return random.choice(boje)
 
-ekran = G.display.set_mode([10 + (širina+2)*q + 200, 10 + (visina+1)*q + 10])
-playground = G.Rect(10, 10, (širina+2)*q, (visina+1)*q)
+class Kraj(Exception): pass
+
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = f'{lijevi_odmak},{gornji_odmak}'
+dimenzije = margina + (širina+2)*q + 200, margina*2 + (visina+1)*q
+ekran = G.display.set_mode(dimenzije)
 G.key.set_repeat(1, 100)
 G.mixer.init()
-G.mixer.music.load('tetrisb.mid')
+G.mixer.music.load('tetrisc.mid')
 G.mixer.music.play(-1)
-# G.font.init()
-# font = G.font.SysFont('Arial', 24, bold=True)
-polje = {Blok(i, 0, Boja.siva) for i in range(16)}
-polje |= {Blok(15, i, Boja.siva) for i in range(1, 11)}
-polje |= {Blok(i, 11, Boja.siva) for i in range(16)}
-
-def kraj():
-    G.key.set_repeat()
-    G.mixer.music.stop()
-    G.quit()
-    print('This is the end, my friend...')
-    print('Bodovi:', bodovi)
-    raise SystemExit
-
-def novi_oblik():
-    o = random.choice(oblici)
-    o.i, o.j = 0, 3
-    print(*o.value, sep='\n')
-    print()
-    o.boja = Boja.slučajna()
-    opolje = o.popuni(o.i, o.j)
-    if opolje.isdisjoint(polje):
-        return o
-    raise Kraj
-    
-class Kraj(Exception):
-    pass
-
-o = None
-score = 0
-G.time.set_timer(G.USEREVENT, 500)
+polje = {Blok(i, 0, Boja.siva) for i in range(1 + visina)} \
+    | {Blok(visina, i, Boja.siva) for i in range(1, 1 + širina)} \
+    | {Blok(i, 1 + širina, Boja.siva) for i in range(1 + visina)}
+o = novi_oblik()
+gravitacija = G.USEREVENT
+G.time.set_timer(gravitacija, 500)
 
 with contextlib.suppress(Kraj):
     while ...:
-        if o is None:
-            o = novi_oblik()
         ekran.fill(Boja.crna)
         for blok in polje: blok.nacrtaj()
-        for blok in o.popuni(o.i, o.j): blok.nacrtaj()
-        if score > 10_000 and not cheat_code_activated:
-            print('Cheat code activated!')
-            print('Press CAPS LOCK...')
-            cheat_code_activated = True
-        ...  # crtanje
+        o.nacrtaj()
+        if bodovi > 10_000 and not cheat:
+            print('Cheat code activated! Press CAPS LOCK...')
+            cheat = True
         G.display.flip()
         for događaj in G.event.get():
-            if događaj.type == G.QUIT:
-                kraj()
-            elif događaj.type == G.USEREVENT:
-                opolje = o.popuni(o.i+1, o.j)
-                if opolje & polje:
-                    polje |= o.popuni(o.i, o.j)
+            if događaj.type == G.QUIT: raise Kraj
+            elif događaj.type == gravitacija:
+                if o.kolizija(1):
+                    konsolidiraj()
                     o = novi_oblik()
-                else: o.i += 1
             elif događaj.type == G.KEYDOWN:
-                if događaj.key == G.K_ESCAPE:
-                    kraj()
-                elif događaj.key == G.K_LEFT:
-                    opolje = o.popuni(o.i, o.j-1)
-                    if opolje.isdisjoint(polje): o.j -= 1
-                elif događaj.key == G.K_RIGHT:
-                    opolje = o.popuni(o.i, o.j+1)
-                    if opolje.isdisjoint(polje): o.j += 1
-                elif događaj.key == G.K_DOWN:
-                    opolje = o.popuni(o.i+1, o.j)
-                    if opolje.isdisjoint(polje): o.i += 1
+                if događaj.key == G.K_ESCAPE: raise Kraj
+                elif događaj.key == G.K_LEFT: o.kolizija(0, -1)
+                elif događaj.key == G.K_RIGHT: o.kolizija(0, 1)
+                elif događaj.key == G.K_DOWN: o.kolizija(1)
                 elif događaj.key == G.K_SPACE:
-                    while ...:
-                        bodovi += 5
-                        opolje = o.popuni(o.i+1, o.j)
-                        if opolje.isdisjoint(polje): o.i += 1
-                        else:
-                            polje |= o.popuni(o.i, o.j)
-                            o = novi_oblik()
-                            break
-                elif događaj.key == G.K_CAPSLOCK and cheat_code_activated:
-                    o.i = 0
-kraj()
+                    while not o.kolizija(1): dodaj_bodove(5)
+                    konsolidiraj()
+                    o = novi_oblik()
+                elif događaj.key == G.K_CAPSLOCK and cheat: o.i = 0
+G.quit()
+print('This is the end, my friend...\nBodovi:', bodovi)
