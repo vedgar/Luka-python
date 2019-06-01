65792e779be3e59aa5eda639efb07a5e158d3149
Veky <vedgar+git@gmail.com>
2019-06-01 16:27:45 +0200

    Sprajtići za fetmena

--- Fatman/Fatman.py
+++ Fatman/Fatman.py
@@ -1,8 +1,8 @@
-import pygame as G, dataclasses, pathlib, enum
+import pygame as G, dataclasses, pathlib, enum, collections
 
 visina, širina = 40, 30
 q = 16
-G.display.set_mode([visina * q, širina * q])
+ekran = G.display.set_mode([visina * q, širina * q])
 sat = G.time.Clock()
 
 def kraj():
@@ -18,6 +18,10 @@ class Tile(enum.Enum):
     VRATA = '_'
     PRAZNO = '\\'
 
+    def __repr__(self):
+        return self.name + self.value
+
+@dataclasses.dataclass
 class Karta:
     poz = (int, int)
     Pacman: poz
@@ -30,7 +34,7 @@ class Karta:
         na, Pacman = {}, set()
         datoteka = pathlib.Path(ime)
         for i, linija in enumerate(open(datoteka)):
-            for j, znak in enumerate(linija):
+            for j, znak in enumerate(linija.strip('\n')):
                 poz = i, j
                 if znak in r'\.0#_': na[poz] = Tile(znak)
                 elif znak == 'C': duhovi['Blinky'].add(poz)
@@ -38,12 +42,28 @@ class Karta:
                 elif znak == 'R': duhovi['Pinky'].add(poz)
                 elif znak == 'N': duhovi['Clyde'].add(poz)
                 elif znak == '%': Pacman.add(poz)
+                else: print('Nepoznat znak', znak)
         return cls(Pacman, duhovi, na)
+
+karta = Karta.iz_datoteke('Fatman.txt')
+galerija = {}
+
+for slika in pathlib.Path('Sprites').iterdir():
+    galerija[slika.stem] = G.image.load(str(slika))
+
+nemaih = set()
 
 while ...:
     sat.tick(20)
+
+    for (i, j), tile in karta.na.items():
+        ime = tile.name.lower()
+        if ime in galerija:
+            ekran.blit(galerija[ime], G.Rect(j * q, i * q, q, q))
+        else: nemaih.add(ime)
+    G.display.flip()
+    
     for događaj in G.event.get():
         if događaj.type == G.QUIT: kraj()
         elif događaj.type == G.KEYDOWN and događaj.key == G.K_ESCAPE: kraj()
         else: print(događaj)
-
